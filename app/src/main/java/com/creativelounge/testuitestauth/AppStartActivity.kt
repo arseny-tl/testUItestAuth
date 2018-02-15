d65d36233e3e01b7626cc4367d44e6e584c9fc65
Arseny Tuhvatullin <wertynsky@gmail.com>
2018-02-15 05:34:41 +0300

    start point to develop

--- app/src/main/java/com/creativelounge/testuitestauth/AppStartActivity.kt
+++ app/src/main/java/com/creativelounge/testuitestauth/AppStartActivity.kt
@@ -2,11 +2,13 @@ package com.creativelounge.testuitestauth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.*
 
 class AppStartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_start)
+
     }
 }
